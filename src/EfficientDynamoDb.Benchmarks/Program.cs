1656e0c352a2dbbef6fd4c8d222a3e700e1d8938
Artur Ispiriants <lezzi1994@gmail.com>
2020-11-14 01:17:17 +0200

    Add lib benchmark project

---
+++ src/EfficientDynamoDb.Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Running;
+using EfficientDynamoDb.DocumentModel;
+using EfficientDynamoDb.DocumentModel.AttributeValues;
+
+namespace EfficientDynamoDb.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromTypes(new[] {typeof(PlaygroundBenchmark)}).RunAll();
+        }
+    }
+}
