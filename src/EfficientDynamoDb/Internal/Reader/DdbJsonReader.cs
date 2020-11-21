93de45d0d7ca6138249a4b0d196a75a244b2d82e
Artur Ispiriants <lezzi1994@gmail.com>
2020-11-22 00:29:34 +0200

    Optimize null support

--- src/EfficientDynamoDb/Internal/Reader/DdbJsonReader.cs
+++ src/EfficientDynamoDb/Internal/Reader/DdbJsonReader.cs
@@ -204,20 +204,31 @@ namespace EfficientDynamoDb.Internal.Reader
         private static void HandleBoolValue(ref DdbReadStack state, bool value)
         {
             ref var current = ref state.GetCurrent();
-            
-            if (current.AttributeType != AttributeType.Unknown)
+
+            switch (current.AttributeType)
             {
-                ref var prevState = ref state.GetPrevious();
+                case AttributeType.Bool:
+                {
+                    ref var prevState = ref state.GetPrevious();
                 
-                prevState.StringBuffer.Add(prevState.KeyName!);
-                prevState.AttributesBuffer.Add(value && current.AttributeType == AttributeType.Null
-                    ? new AttributeValue(new NullAttributeValue(true))
-                    : new AttributeValue(new BoolAttributeValue(value)));
-            }
-            else
-            {
-                current.StringBuffer.Add(current.KeyName!);
-                current.AttributesBuffer.Add(new AttributeValue(new BoolAttributeValue(value)));
+                    prevState.StringBuffer.Add(prevState.KeyName!);
+                    prevState.AttributesBuffer.Add(new AttributeValue(new BoolAttributeValue(value)));
+                    break;
+                }
+                case AttributeType.Null:
+                {
+                    ref var prevState = ref state.GetPrevious();
+                
+                    prevState.StringBuffer.Add(prevState.KeyName!);
+                    prevState.AttributesBuffer.Add(new AttributeValue(new BoolAttributeValue(value)));
+                    break;
+                }
+                default:
+                {
+                    current.StringBuffer.Add(current.KeyName!);
+                    current.AttributesBuffer.Add(new AttributeValue(new BoolAttributeValue(value)));
+                    break;
+                }
             }
         }
         
