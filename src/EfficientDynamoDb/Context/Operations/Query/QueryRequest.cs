4f4a1f0697b69d2fff8f30d0719dd5bf3484acde
Artur Ispiriants <lezzi1994@gmail.com>
2020-11-24 03:21:09 +0200

    Scan request/response

--- src/EfficientDynamoDb/Context/Operations/Query/QueryRequest.cs
+++ src/EfficientDynamoDb/Context/Operations/Query/QueryRequest.cs
@@ -4,95 +4,14 @@ using EfficientDynamoDb.DocumentModel.ReturnDataFlags;
 
 namespace EfficientDynamoDb.Context.Operations.Query
 {
-    public class QueryRequest
+    public class QueryRequest : IterableRequest
     {
-        /// <summary>
-        /// The name of the table containing the requested items.
-        /// </summary>
-        public string? TableName { get; set; }
-        
-        /// <summary>
-        /// The name of an index to query. This index can be any local secondary index or global secondary index on the table. Note that if you use the <see cref="IndexName"/> parameter, you must also provide <see cref="TableName"/>.
-        /// </summary>
-        public string? IndexName { get; set; }
-
-        /// <summary>
-        /// Determines the read consistency model: If set to true, then the operation uses strongly consistent reads; otherwise, the operation uses eventually consistent reads. <br/><br/>
-        /// Strongly consistent reads are not supported on global secondary indexes. If you query a global secondary index with <see cref="ConsistentRead"/> set to true, you will receive a validation exception.
-        /// </summary>
-        public bool ConsistentRead { get; set; }
-
-        /// <summary>
-        /// The maximum number of items to evaluate (not necessarily the number of matching items). If DynamoDB processes the number of items up to the limit while processing the results, it stops the operation and returns the matching values up to that point, and a key in LastEvaluatedKey to apply in a subsequent operation, so that you can pick up where you left off. Also, if the processed dataset size exceeds 1 MB before DynamoDB reaches this limit, it stops the operation and returns the matching values up to the limit, and a key in <see cref="LastEvaluatedKey"/> to apply in a subsequent operation to continue the operation.
-        /// </summary>
-        public int? Limit { get; set; }
-        
-        /// <summary>
-        /// A collection of strings that identifies one or more attributes to retrieve from the table. These attributes can include scalars, sets, or elements of a JSON document. <br/><br/>
-        /// If no attribute names are specified, then all attributes will be returned. If any of the requested attributes are not found, they will not appear in the result. 
-        /// </summary>
-        public IReadOnlyList<string>? ProjectionExpression { get; set; }
-
         /// <summary>
         /// Specifies the order for index traversal: If true (default), the traversal is performed in ascending order; if false, the traversal is performed in descending order.<br/><br/>
         /// Items with the same partition key value are stored in sorted order by sort key. If the sort key data type is Number, the results are stored in numeric order. For type String, the results are stored in order of UTF-8 bytes. For type Binary, DynamoDB treats each byte of the binary data as unsigned.<br/><br/>
         /// If <see cref="ScanIndexForward"/> is true, DynamoDB returns the results in the order in which they are stored (by sort key value). This is the default behavior. If <see cref="ScanIndexForward"/> is false, DynamoDB reads the results in reverse order by sort key value, and then returns the results to the client.
         /// </summary>
         public bool ScanIndexForward { get; set; } = true;
-        
-        /// <summary>
-        /// Determines the level of detail about provisioned throughput consumption that is returned in the response. <br/><br/>
-        /// </summary>
-        public ReturnConsumedCapacity ReturnConsumedCapacity { get; set; }
-        
-        /// <summary>
-        /// The attributes to be returned in the result. You can retrieve all item attributes, specific item attributes, the count of matching items, or in the case of an index, some or all of the attributes projected into the index.
-        /// </summary>
-        public Select? Select { get; set; }
-        
-        /// <summary>
-        /// The primary key of the first item that this operation will evaluate. Use the value that was returned for LastEvaluatedKey in the previous operation.
-        /// </summary>
-        public IReadOnlyDictionary<string, AttributeValue>? ExclusiveStartKey { get; set; }
-        
-        /// <summary>
-        /// One or more substitution tokens for attribute names in an expression. The following are some use cases for using ExpressionAttributeNames:
-        /// <list type="bullet">
-        /// <listheader>
-        /// <description> Valid comparisons for the sort key condition are as follows:</description>
-        /// </listheader>
-        /// <item>
-        /// <term>To access an attribute whose name conflicts with a DynamoDB reserved word.</term>
-        /// </item>
-        /// <item>
-        /// <term>To create a placeholder for repeating occurrences of an attribute name in an expression.</term>
-        /// </item>
-        /// <item>
-        /// <term>To prevent special characters in an attribute name from being misinterpreted in an expression.</term>
-        /// </item>
-        /// </list>
-        /// Use the <c>#</c> character in a <see cref="KeyConditionExpression"/> or <see cref="FilterExpression"/> to dereference an attribute name.<br/>
-        /// <example> If your <see cref="KeyConditionExpression"/> is equal to <c>#pk = :pk</c>, then map <c>#pk</c> to the real attribute name in the <see cref="ExpressionAttributeNames"/> dictionary
-        /// <code>AttributeNames = new Dictionary&lt;string, string>
-        /// {
-        ///     ["#pk"] = "pk"
-        /// }
-        /// </code>
-        /// </example>
-        /// </summary>
-        public IReadOnlyDictionary<string, string>? ExpressionAttributeNames { get; set; }
-        
-        /// <summary>
-        /// One or more values that can be substituted in an expression. Use the <c>:</c> (colon) character in an expression to dereference an attribute value.<br/>
-        /// <example>If your <see cref="KeyConditionExpression"/> is equal to <c>#pk = :pk</c>, then map <c>:pk</c> to the real attribute value in the <see cref="ExpressionAttributeValues"/> dictionary:
-        /// <code>AttributeValues = new Dictionary&lt;string, AttributeValue>
-        /// {
-        ///     [":pk"] = pkValue
-        /// }
-        /// </code>
-        /// </example>
-        /// </summary>
-        public IReadOnlyDictionary<string, AttributeValue>? ExpressionAttributeValues { get; set; }
         
         /// <summary>
         /// The condition that specifies the key values for items to be retrieved by the Query action. <br/><br/>
---
+++ src/EfficientDynamoDb/Context/Operations/Scan/ScanRequest.cs
@@ -0,0 +1,35 @@
+using System;
+using EfficientDynamoDb.Context.Operations.Query;
+
+namespace EfficientDynamoDb.Context.Operations.Scan
+{
+    public class ScanRequest : IterableRequest
+    {
+        /// <summary>
+        /// The name of the table containing the requested items; or, if you provide IndexName, the name of the table to which that index belongs.
+        /// </summary>
+        public string? TableName { get; set; }
+        
+        /// <summary>
+        /// A string that contains conditions that DynamoDB applies after the Scan operation, but before the data is returned to you. Items that do not satisfy the <see cref="FilterExpression"/> criteria are not returned.<br/><br/>
+        /// A <see cref="FilterExpression"/> is applied after the items have already been read; the process of filtering does not consume any additional read capacity units.
+        /// </summary>
+        public string? FilterExpression { get; set; }
+        
+        /// <summary>
+        /// For a parallel Scan request, <see cref="Segment"/> identifies an individual segment to be scanned by an application worker.<br/><br/>
+        /// <see cref="Segment"/> IDs are zero-based, so the first segment is always 0. For example, if you want to use four application threads to scan a table or an index, then the first thread specifies a <see cref="Segment"/> value of 0, the second thread specifies 1, and so on.<br/><br/>
+        /// The value of <see cref="IterableResponse.LastEvaluatedKey"/> returned from a parallel Scan request must be used as <see cref="IterableRequest.ExclusiveStartKey"/> with the same segment ID in a subsequent Scan operation.<br/><br/>
+        /// The value for <see cref="Segment"/> must be greater than or equal to 0, and less than the value provided for TotalSegments.<br/><br/>
+        /// If you provide <see cref="Segment"/>, you must also provide TotalSegments.
+        /// </summary>
+        public int? Segment { get; set; }
+        
+        /// <summary>
+        /// For a parallel Scan request, <see cref="TotalSegments"/> represents the total number of segments into which the Scan operation will be divided. The value of <see cref="TotalSegments"/> corresponds to the number of application workers that will perform the parallel scan. For example, if you want to use four application threads to scan a table or an index, specify a <see cref="TotalSegments"/> value of 4.<br/><br/>
+        /// The value for <see cref="TotalSegments"/> must be greater than or equal to 1, and less than or equal to 1000000. If you specify a <see cref="TotalSegments"/> value of 1, the Scan operation will be sequential rather than parallel.<br/><br/>
+        /// If you specify <see cref="TotalSegments"/>, you must also specify <see cref="Segment"/>.
+        /// </summary>
+        public int? TotalSegments { get; set; }
+    }
+}
