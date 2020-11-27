748a4cb3c515e6a00dc23cfd941a07ef7cdd367f
Artur Ispiriants <lezzi1994@gmail.com>
2020-11-27 03:24:58 +0200

    Fix memory stream nuget

--- src/EfficientDynamoDb/Context/Operations/TransactWriteItems/TransactWriteItem.cs
+++ src/EfficientDynamoDb/Context/Operations/TransactWriteItems/TransactWriteItem.cs
@@ -8,21 +8,54 @@ namespace EfficientDynamoDb.Context.Operations.TransactWriteItems
         /// <summary>
         /// A request to perform a check item operation.
         /// </summary>
-        public ConditionCheck? ConditionCheck { get; set; }
+        public ConditionCheck? ConditionCheck { get; }
         
         /// <summary>
         /// A request to perform a DeleteItem operation.
         /// </summary>
-        public TransactDeleteItem? Delete { get; set; }
+        public TransactDeleteItem? Delete { get; }
         
         /// <summary>
         /// A request to perform a PutItem operation.
         /// </summary>
-        public TransactPutItem? Put { get; set; }
+        public TransactPutItem? Put { get; }
         
         /// <summary>
         /// A request to perform an UpdateItem operation.
         /// </summary>
-        public TransactUpdateItem? Update { get; set; }
+        public TransactUpdateItem? Update { get; }
+
+        public TransactWriteItem(TransactDeleteItem? delete, ConditionCheck? conditionCheck)
+        {
+            ConditionCheck = conditionCheck;
+            Delete = delete;
+        }
+
+        public TransactWriteItem(TransactDeleteItem? delete)
+        {
+            Delete = delete;
+        }
+
+        public TransactWriteItem(TransactPutItem? put)
+        {
+            Put = put;
+        }
+
+        public TransactWriteItem(TransactPutItem? put, ConditionCheck? conditionCheck)
+        {
+            ConditionCheck = conditionCheck;
+            Put = put;
+        }
+
+        public TransactWriteItem(TransactUpdateItem? update)
+        {
+            Update = update;
+        }
+
+        public TransactWriteItem(TransactUpdateItem? update, ConditionCheck? conditionCheck)
+        {
+            ConditionCheck = conditionCheck;
+            Update = update;
+        }
     }
 }
