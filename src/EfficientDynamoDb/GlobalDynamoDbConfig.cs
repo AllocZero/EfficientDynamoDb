6f5ca89b217d1daa2eba7e44bb9e6e1b48be294f
Artur Ispiriants <lezzi1994@gmail.com>
2020-12-03 22:22:59 +0200

    Cleanup

--- src/EfficientDynamoDb/GlobalDynamoDbConfig.cs
+++ src/EfficientDynamoDb/GlobalDynamoDbConfig.cs
@@ -6,10 +6,5 @@ namespace EfficientDynamoDb
         /// Uses custom memory stream implemenation (does not use ArrayPool).
         /// </summary>
         public static bool UseMemoryStreamPooling { get; set; } = true;
-
-        /// <summary>
-        /// Pooled buffer writer gives a huge performance boost and reduces memory allocations.
-        /// </summary>
-        public static bool UsePooledBufferForJsonWrites { get; set; } = true;
     }
 }
