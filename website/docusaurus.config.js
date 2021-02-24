1cb460207360f0c4850250fe2e88f1a372da82f3
Mykhailo Matviiv <firenero94@gmail.com>
2021-02-24 04:10:26 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://alloczero.github.io',
-  baseUrl: '/efficientdynamodb',
+  baseUrl: '/efficientdynamodb/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
