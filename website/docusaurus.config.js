81aa85b422998856aac16801b7abba464a86d5cf
Mykhailo Matviiv <firenero94@gmail.com>
2021-02-24 04:10:12 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://alloczero.github.io',
+  baseUrl: '/efficientdynamodb',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AllocZero', // Usually your GitHub org/user name.
+  projectName: 'EfficientDynamoDb', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
