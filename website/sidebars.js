ba502c2d673db5a223175d78dd430ac550ff2b67
Mykhailo Matviiv <firenero94@gmail.com>
2021-03-02 03:28:55 +0200

    Fix docs sidebar (#33)

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,18 @@
 module.exports = {
   someSidebar: {
-    EfficientDynamoDb: ['efficient_dynamo_db/introduction', 'efficient_dynamo_db/design'],
-    'Getting Started': ['getting_started/installation']
+    EfficientDynamoDb: [
+      'efficient_dynamo_db/introduction', 
+      'efficient_dynamo_db/design'
+    ],
+    'Getting Started': [
+      'getting_started/installation'
+    ],
+    'Developer Guide': [
+      {
+        Initialization: [
+          'dev_guide/setup'
+        ]
+      }
+    ]
   },
 };
