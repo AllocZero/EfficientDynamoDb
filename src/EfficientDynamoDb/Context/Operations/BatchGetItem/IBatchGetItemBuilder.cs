61ce9bd49cef60c1c1d6ec9af5b0acddd58a7afa
Artur Ispiriants <lezzi1994@gmail.com>
2021-03-06 02:42:47 +0200

    Make get builder internal

--- src/EfficientDynamoDb/Context/Operations/BatchGetItem/IBatchGetItemBuilder.cs
+++ src/EfficientDynamoDb/Context/Operations/BatchGetItem/IBatchGetItemBuilder.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using EfficientDynamoDb.Context.Operations.Query;
-using EfficientDynamoDb.DocumentModel;
 
 namespace EfficientDynamoDb.Context.Operations.BatchGetItem
 {
