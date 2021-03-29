fd7ac6d9021beabeb92e5dda545fa06aa623674a
Artur Ispiriants <lezzi1994@gmail.com>
2021-03-29 03:31:39 +0300

    Remove unused namespace

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Text.Json;
 using BenchmarkDotNet.Running;
 
 namespace Benchmarks
