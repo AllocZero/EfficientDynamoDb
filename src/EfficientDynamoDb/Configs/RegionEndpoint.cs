73cbaa20f7f89fcec66f9671ed83a67c6c0abd7b
MarkPflug <mark.pflug@live.com>
2021-04-01 10:06:03 -0700

    allow specifying custom request url to support local dynamo docker container

--- src/EfficientDynamoDb/Configs/RegionEndpoint.cs
+++ src/EfficientDynamoDb/Configs/RegionEndpoint.cs
@@ -10,136 +10,141 @@ namespace EfficientDynamoDb.Configs
         internal string RequestUri { get; }
         
         public string Region { get; }
+
+        static RegionEndpoint Create(string region, string uriFormat)
+        {
+            return new RegionEndpoint(region, string.Format(uriFormat, ServiceName, region));
+        }
         
-        private RegionEndpoint(string region, string uriFormat)
+        public RegionEndpoint(string region, string requestUri)
         {
             Region = region;
-            RequestUri = string.Format(uriFormat, ServiceName, region);
+            RequestUri = requestUri;
         }
 
         /// <summary>
         /// The US East (N. Virginia) endpoint.
         /// </summary>
-        public static RegionEndpoint USEast1 => new RegionEndpoint("us-east-1", RegularEndpointFormat);
+        public static RegionEndpoint USEast1 => Create("us-east-1", RegularEndpointFormat);
         
         /// <summary>
         /// The US East (Ohio) endpoint.
         /// </summary>
-        public static RegionEndpoint USEast2 => new RegionEndpoint("us-east-2", RegularEndpointFormat);
+        public static RegionEndpoint USEast2 => Create("us-east-2", RegularEndpointFormat);
         
         /// <summary>
         /// The US West (N. California) endpoint.
         /// </summary>
-        public static RegionEndpoint USWest1 => new RegionEndpoint("us-west-1", RegularEndpointFormat);
+        public static RegionEndpoint USWest1 => Create("us-west-1", RegularEndpointFormat);
         
         /// <summary>
         /// The US West (Oregon) endpoint.
         /// </summary>
-        public static RegionEndpoint USWest2 => new RegionEndpoint("us-west-2", RegularEndpointFormat);
+        public static RegionEndpoint USWest2 => Create("us-west-2", RegularEndpointFormat);
         
         /// <summary>
         /// The Africa (Cape Town) endpoint.
         /// </summary>
-        public static RegionEndpoint AFSouth1 => new RegionEndpoint("af-south-1", RegularEndpointFormat);
+        public static RegionEndpoint AFSouth1 => Create("af-south-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Hong Kong) endpoint.
         /// </summary>
-        public static RegionEndpoint APEast1 => new RegionEndpoint("ap-east-1", RegularEndpointFormat);
+        public static RegionEndpoint APEast1 => Create("ap-east-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Mumbai) endpoint.
         /// </summary>
-        public static RegionEndpoint APSouth1 => new RegionEndpoint("ap-south-1", RegularEndpointFormat);
+        public static RegionEndpoint APSouth1 => Create("ap-south-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Tokyo) endpoint.
         /// </summary>
-        public static RegionEndpoint APNorthEast1 => new RegionEndpoint("ap-northeast-1", RegularEndpointFormat);
+        public static RegionEndpoint APNorthEast1 => Create("ap-northeast-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Seoul) endpoint.
         /// </summary>
-        public static RegionEndpoint APNorthEast2 => new RegionEndpoint("ap-northeast-2", RegularEndpointFormat);
+        public static RegionEndpoint APNorthEast2 => Create("ap-northeast-2", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Osaka-Local) endpoint.
         /// </summary>
-        public static RegionEndpoint APNorthEast3 => new RegionEndpoint("ap-northeast-3", RegularEndpointFormat);
+        public static RegionEndpoint APNorthEast3 => Create("ap-northeast-3", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Singapore) endpoint.
         /// </summary>
-        public static RegionEndpoint APSouthEast1 => new RegionEndpoint("ap-southeast-1", RegularEndpointFormat);
+        public static RegionEndpoint APSouthEast1 => Create("ap-southeast-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Asia Pacific (Sydney) endpoint.
         /// </summary>
-        public static RegionEndpoint APSouthEast2 => new RegionEndpoint("ap-southeast-2", RegularEndpointFormat);
+        public static RegionEndpoint APSouthEast2 => Create("ap-southeast-2", RegularEndpointFormat);
         
         /// <summary>
         /// The Canada (Central) endpoint.
         /// </summary>
-        public static RegionEndpoint CACentral1 => new RegionEndpoint("ca-central-1", RegularEndpointFormat);
+        public static RegionEndpoint CACentral1 => Create("ca-central-1", RegularEndpointFormat);
         
         /// <summary>
         /// The China (Beijing) endpoint.
         /// </summary>
-        public static RegionEndpoint CNNorth1 => new RegionEndpoint("cn-north-1", ChinaEndpointFormat);
+        public static RegionEndpoint CNNorth1 => Create("cn-north-1", ChinaEndpointFormat);
         
         /// <summary>
         /// The China (Ningxia) endpoint.
         /// </summary>
-        public static RegionEndpoint CNNorthWest1 => new RegionEndpoint("cn-northwest-1", ChinaEndpointFormat);
+        public static RegionEndpoint CNNorthWest1 => Create("cn-northwest-1", ChinaEndpointFormat);
         
         /// <summary>
         /// The Europe (Frankfurt) endpoint.
         /// </summary>
-        public static RegionEndpoint EUCenteral1 => new RegionEndpoint("eu-central-1", RegularEndpointFormat);
+        public static RegionEndpoint EUCenteral1 => Create("eu-central-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Europe (Ireland) endpoint.
         /// </summary>
-        public static RegionEndpoint EUWest1 => new RegionEndpoint("eu-west-1", RegularEndpointFormat);
+        public static RegionEndpoint EUWest1 => Create("eu-west-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Europe (London) endpoint.
         /// </summary>
-        public static RegionEndpoint EUWest2 => new RegionEndpoint("eu-west-2", RegularEndpointFormat);
+        public static RegionEndpoint EUWest2 => Create("eu-west-2", RegularEndpointFormat);
         
         /// <summary>
         /// The Europe (Paris) endpoint.
         /// </summary>
-        public static RegionEndpoint EUWest3 => new RegionEndpoint("eu-west-3", RegularEndpointFormat);
+        public static RegionEndpoint EUWest3 => Create("eu-west-3", RegularEndpointFormat);
         
         /// <summary>
         /// The Europe (Milan) endpoint.
         /// </summary>
-        public static RegionEndpoint EUSouth1 => new RegionEndpoint("eu-south-1", RegularEndpointFormat);
+        public static RegionEndpoint EUSouth1 => Create("eu-south-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Europe (Stockholm) endpoint.
         /// </summary>
-        public static RegionEndpoint EUNorth1 => new RegionEndpoint("eu-north-1", RegularEndpointFormat);
+        public static RegionEndpoint EUNorth1 => Create("eu-north-1", RegularEndpointFormat);
         
         /// <summary>
         /// The Middle East (Bahrain) endpoint.
         /// </summary>
-        public static RegionEndpoint MESouth1 => new RegionEndpoint("me-south-1", RegularEndpointFormat);
+        public static RegionEndpoint MESouth1 => Create("me-south-1", RegularEndpointFormat);
         
         /// <summary>
         /// The South America (São Paulo) endpoint.
         /// </summary>
-        public static RegionEndpoint SAEast1 => new RegionEndpoint("sa-east-1", RegularEndpointFormat);
+        public static RegionEndpoint SAEast1 => Create("sa-east-1", RegularEndpointFormat);
         
         /// <summary>
         /// The AWS GovCloud (US-East) endpoint.
         /// </summary>
-        public static RegionEndpoint USGovEast1 => new RegionEndpoint("us-gov-east-1", RegularEndpointFormat);
+        public static RegionEndpoint USGovEast1 => Create("us-gov-east-1", RegularEndpointFormat);
         
         /// <summary>
         /// The AWS GovCloud (US) endpoint.
         /// </summary>
-        public static RegionEndpoint USGovWest1 => new RegionEndpoint("us-gov-west-1", RegularEndpointFormat);
+        public static RegionEndpoint USGovWest1 => Create("us-gov-west-1", RegularEndpointFormat);
     }
 }
