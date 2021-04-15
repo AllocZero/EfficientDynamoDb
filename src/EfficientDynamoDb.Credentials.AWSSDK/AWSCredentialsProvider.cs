37a89b6f9495f70c87fc8712ec19bd98535fc8ce
Artur Ispiriants <lezzi1994@gmail.com>
2021-04-16 02:51:04 +0300

    AWSSDK Credentials integration project

---
+++ src/EfficientDynamoDb.Credentials.AWSSDK/AWSCredentialsProvider.cs
@@ -0,0 +1,24 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+using EfficientDynamoDb.Configs;
+
+namespace EfficientDynamoDb.Credentials.AWSSDK
+{
+    public class AWSCredentialsProvider : IAwsCredentialsProvider
+    {
+        private readonly AWSCredentials _awsCredentials;
+
+        public AWSCredentialsProvider(AWSCredentials awsCredentials)
+        {
+            _awsCredentials = awsCredentials;
+        }
+
+        public async ValueTask<AwsCredentials> GetCredentialsAsync(CancellationToken cancellationToken = default)
+        {
+            var credentials = await _awsCredentials.GetCredentialsAsync().ConfigureAwait(false);
+
+            return new AwsCredentials(credentials.AccessKey, credentials.SecretKey, credentials.Token);
+        }
+    }
+}
