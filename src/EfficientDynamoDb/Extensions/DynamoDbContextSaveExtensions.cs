4e7a1797e786ac5cc44ea2a9e8f26889fab4e284
Mykhailo Matviiv <firenero94@gmail.com>
2021-07-19 00:27:56 +0300

    Add xml docs for SaveAsync and DeleteAsync compatibility methods

--- src/EfficientDynamoDb/Extensions/DynamoDbContextSaveExtensions.cs
+++ src/EfficientDynamoDb/Extensions/DynamoDbContextSaveExtensions.cs
@@ -7,6 +7,17 @@ namespace EfficientDynamoDb.Extensions
 {
     public static class DynamoDbContextSaveExtensions
     {
+        /// <summary>
+        /// Compatibility method. Deletes an entity in a way that AWS .NET SDK does.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <remarks>
+        /// Check DeleteAsync compatibility section for details:
+        /// https://alloczero.github.io/EfficientDynamoDb/docs/dev-guide/sdk-compatibility#operations
+        /// </remarks>
         public static async Task DeleteAsync<TEntity>(this IDynamoDbContext context, TEntity entity, CancellationToken cancellationToken = default) where TEntity : class
         {
             using var httpContent = new DeleteEntityHighLevelHttpContent<TEntity>(context.Config, entity);
@@ -14,6 +25,17 @@ namespace EfficientDynamoDb.Extensions
             await context.ExecuteAsync<object>(httpContent, cancellationToken).ConfigureAwait(false);
         }
         
+        /// <summary>
+        /// Compatibility method. Saves an entity in a way that AWS .NET SDK does.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <remarks>
+        /// Check SaveAsync compatibility section for details:
+        /// https://alloczero.github.io/EfficientDynamoDb/docs/dev-guide/sdk-compatibility#operations
+        /// </remarks>
         public static async Task SaveAsync<TEntity>(this IDynamoDbContext context, TEntity entity, CancellationToken cancellationToken = default) where TEntity : class
         {
             using var httpContent = new UpdateItemSaveHttpContent<TEntity>(context.Config, entity);
