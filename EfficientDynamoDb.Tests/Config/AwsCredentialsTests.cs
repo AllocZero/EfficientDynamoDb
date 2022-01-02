58cb63b48987ece8325c2b74d304023fed090a52
Newp <mynewp@gmail.com>
2022-01-03 07:39:26 +0900

    fix wrong scenario

--- EfficientDynamoDb.Tests/Config/AwsCredentialsTests.cs
+++ EfficientDynamoDb.Tests/Config/AwsCredentialsTests.cs
@@ -12,15 +12,30 @@ namespace EfficientDynamoDb.Tests.Config.Retries
     [TestFixture]
     public class AwsCredentialsTests
     {
+        [SetUp]
+        public void ClearEnvironmentVariables()
+        {
+            var keys = new[]
+             {
+                    "AWS_ACCESS_KEY_ID",
+                    "AWS_SECRET_ACCESS_KEY",
+                    "AWS_SESSION_TOKEN",
+                };
+            foreach (var key in keys)
+            {
+                Environment.SetEnvironmentVariable(key, null, EnvironmentVariableTarget.Process);
+            }
+
+        }
 
         [Test]
         public void GetFromEnvironmentTest()
         {
             var keys = new[]
              {
-                "AWS_ACCESS_KEY_ID",
-                "AWS_SECRET_ACCESS_KEY"
-            };
+                    "AWS_ACCESS_KEY_ID",
+                    "AWS_SECRET_ACCESS_KEY"
+                };
             foreach (var key in keys)
             {
                 Environment.SetEnvironmentVariable(key, "TEST###" + key, EnvironmentVariableTarget.Process);
@@ -29,20 +44,21 @@ namespace EfficientDynamoDb.Tests.Config.Retries
             var result = AwsCredentials.GetAwsCredentials();
 
             Assert.NotNull(result);
-            Assert.Equals(keys[0], result.AccessKey);
-            Assert.Equals(keys[1], result.SecretKey);
-            Assert.IsNull(result.Token);
+            Assert.Equals("TEST###" + keys[0], result.AccessKey);
+            Assert.Equals("TEST###" + keys[1], result.SecretKey);
+            Assert.Null(result.Token);
         }
+
 
         [Test]
         public void GetFromEnvironmentTestWithToken()
         {
             var keys = new[]
              {
-                "AWS_ACCESS_KEY_ID",
-                "AWS_SECRET_ACCESS_KEY",
-                "AWS_SESSION_TOKEN",
-            };
+                    "AWS_ACCESS_KEY_ID",
+                    "AWS_SECRET_ACCESS_KEY",
+                    "AWS_SESSION_TOKEN",
+                };
             foreach (var key in keys)
             {
                 Environment.SetEnvironmentVariable(key, "TEST###" + key, EnvironmentVariableTarget.Process);
@@ -51,21 +67,21 @@ namespace EfficientDynamoDb.Tests.Config.Retries
             var result = AwsCredentials.GetAwsCredentials();
 
             Assert.NotNull(result);
-            Assert.Equals(keys[0], result.AccessKey);
-            Assert.Equals(keys[1], result.SecretKey);
-            Assert.Equals(keys[2], result.Token);
+            Assert.Equals("TEST###" + keys[0], result.AccessKey);
+            Assert.Equals("TEST###" + keys[1], result.SecretKey);
+            Assert.Equals("TEST###" + keys[2], result.Token);
         }
-
-
 
         [Test]
         public void GetNullFromEnvironment()
         {
-            Assert.IsNull(AwsCredentials.GetAwsCredentials());
+            Assert.Null(AwsCredentials.GetAwsCredentials());
             var result = AwsCredentials.GetAwsCredentials();
         }
 
     }
+
+}
 
     
 
