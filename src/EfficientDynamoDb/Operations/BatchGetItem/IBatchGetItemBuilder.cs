49fbe297da315f79b02fb616ec857784ec94e863
Mykhailo Matviiv <firenero94@gmail.com>
2023-05-21 17:43:14 +0200

    XML docs for Batch utility class

--- src/EfficientDynamoDb/Operations/BatchGetItem/IBatchGetItemBuilder.cs
+++ src/EfficientDynamoDb/Operations/BatchGetItem/IBatchGetItemBuilder.cs
@@ -3,6 +3,9 @@ using EfficientDynamoDb.Operations.Query;
 
 namespace EfficientDynamoDb.Operations.BatchGetItem
 {
+    /// <summary>
+    /// Defines the contract for building a GetItem request in a batch.
+    /// </summary>
     public interface IBatchGetItemBuilder
     {
         internal PrimaryKeyNodeBase GetPrimaryKeyNode();
@@ -13,8 +16,30 @@ namespace EfficientDynamoDb.Operations.BatchGetItem
 
         internal IBatchGetItemBuilder WithTableName(string tableName);
         
+        /// <summary>
+        /// Specifies the partition key of the item to get in batch.
+        /// </summary>
+        /// <param name="pk">Partition key of the item.</param>
+        /// <typeparam name="TPk">Type of the partition key.</typeparam>
+        /// <returns>BatchGetItem operation builder.</returns>
+        /// <remarks>
+        /// This method should be used only if the table has only partition key.
+        /// If the table has both partition and sort keys, use <see cref="WithPrimaryKey{TPk,TSk}"/> instead.
+        /// </remarks>
         IBatchGetItemBuilder WithPrimaryKey<TPk>(TPk pk);
         
+        /// <summary>
+        /// Specifies partition and sort keys of the item to get in batch.
+        /// </summary>
+        /// <param name="pk">Partition key of the item.</param>
+        /// <param name="sk">Sort key of the item.</param>
+        /// <typeparam name="TPk">Type of the partition key.</typeparam>
+        /// <typeparam name="TSk">Type of the sort key.</typeparam>
+        /// <returns>BatchGetItem operation builder.</returns>
+        /// <remarks>
+        /// This method should be used only if the table has both partition and sort keys.
+        /// If the table has only partition key, use <see cref="WithPrimaryKey{TPk}"/> instead.
+        /// </remarks>
         IBatchGetItemBuilder WithPrimaryKey<TPk, TSk>(TPk pk, TSk sk);
     }
 }
