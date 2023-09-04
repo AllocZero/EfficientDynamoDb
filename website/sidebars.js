208dfe60e96db94188d6c967e745581ab30a448e
Mykhailo Matviiv <firenero94@gmail.com>
2023-09-04 20:42:26 +0200

    Add API reference docs for Select mode

--- website/sidebars.js
+++ website/sidebars.js
@@ -32,7 +32,10 @@ module.exports = {
       {
         'Request Builders': [
           'api_reference/builders/get-item-builder',
-          'api_reference/builders/query-builder'
+          'api_reference/builders/query-builder',
+        ],
+        'Options': [
+          'api_reference/options/select-mode',
         ],
       },
       'api_reference/scan',
