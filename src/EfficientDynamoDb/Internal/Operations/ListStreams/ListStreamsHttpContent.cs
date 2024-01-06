fcf493d146c16c00bd8db84737f6a1f47d8ed65c
Mykhailo Matviiv <firenero94@gmail.com>
2024-01-06 16:25:21 +0100

    ListStreams API

---
+++ src/EfficientDynamoDb/Internal/Operations/ListStreams/ListStreamsHttpContent.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using EfficientDynamoDb.Converters;
+using EfficientDynamoDb.Internal.Extensions;
+using EfficientDynamoDb.Internal.Operations.Shared;
+using EfficientDynamoDb.Operations;
+
+namespace EfficientDynamoDb.Internal.Operations.ListStreams
+{
+    internal class ListStreamsHttpContent : DynamoDbHttpContent
+    {
+        private readonly ListStreamsRequest _request;
+        private readonly string? _tablePrefix;
+
+        public ListStreamsHttpContent(ListStreamsRequest request, string? tablePrefix) : base("DynamoDBStreams_20120810.ListStreams")
+        {
+            _request = request;
+            _tablePrefix = tablePrefix;
+        }
+
+        protected override ValueTask WriteDataAsync(DdbWriter ddbWriter)
+        {
+            var writer = ddbWriter.JsonWriter;
+            writer.WriteStartObject();
+            
+            if (_request.ExclusiveStartStreamArn != null)
+                writer.WriteString("ExclusiveStartStreamArn", _request.ExclusiveStartStreamArn);
+            
+            if (_request.Limit > 0)
+                writer.WriteNumber("Limit", _request.Limit);
+
+            if (_request.TableName != null)
+                writer.WriteTableName(_tablePrefix, _request.TableName);
+            
+            writer.WriteEndObject();
+
+            return default;
+        }
+    }
+}
---
+++ src/EfficientDynamoDb/Operations/ListStreams/ListStreamsRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace EfficientDynamoDb.Operations
+{
+    public class ListStreamsRequest
+    {
+        /// <summary>
+        /// The ARN (Amazon Resource Name) of the first item that this operation will evaluate.
+        /// Use the value that was returned for <see cref="ListStreamsResponse.LastEvaluatedStreamArn"/> in the previous operation.
+        /// </summary>
+        public string? ExclusiveStartStreamArn { get; set; }
+
+        /// <summary>
+        /// The maximum number of streams to return.
+        /// Values less or equal to zero are interpreted as no limit.
+        /// The upper limit is 100.
+        /// </summary>
+        public int Limit { get; set; } = -1;
+        
+        /// <summary>
+        /// If this parameter is provided, then only the streams associated with this table name are returned.
+        /// </summary>
+        public string? TableName { get; set; }
+    }
+}
