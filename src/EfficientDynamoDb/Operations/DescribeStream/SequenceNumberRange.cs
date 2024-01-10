e9d53eed6b3f23db35d77771d33d8b43eda0b2d7
Mykhailo Matviiv <firenero94@gmail.com>
2024-01-10 02:02:25 +0100

    DescribeStream API

---
+++ src/EfficientDynamoDb/Operations/DescribeStream/SequenceNumberRange.cs
@@ -0,0 +1,23 @@
+﻿namespace EfficientDynamoDb.Operations
+{
+    public class SequenceNumberRange
+    {
+        /// <summary>
+        /// The first sequence number for the stream records contained within a shard.
+        /// String contains numeric characters only.
+        /// </summary>
+        public string StartingSequenceNumber { get; }
+        
+        /// <summary>
+        /// The last sequence number for the stream records contained within a shard.
+        /// String contains numeric characters only.
+        /// </summary>
+        public string EndingSequenceNumber { get; }
+        
+        public SequenceNumberRange(string startingSequenceNumber, string endingSequenceNumber)
+        {
+            StartingSequenceNumber = startingSequenceNumber;
+            EndingSequenceNumber = endingSequenceNumber;
+        }
+    }
+}
