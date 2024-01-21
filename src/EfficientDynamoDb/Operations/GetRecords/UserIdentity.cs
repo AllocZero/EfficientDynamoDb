1e66d11922c36b5ea75c28c29feee4d204223d09
Mykhailo Matviiv <firenero94@gmail.com>
2024-01-21 16:12:12 +0100

    GetRecords API

---
+++ src/EfficientDynamoDb/Operations/GetRecords/UserIdentity.cs
@@ -0,0 +1,20 @@
+﻿namespace EfficientDynamoDb.Operations
+{
+    /// <summary>
+    /// Contains details about the type of identity that made the request.
+    /// </summary>
+    public class UserIdentity
+    {
+        /// <summary>
+        /// A unique identifier for the entity that made the call.
+        /// For Time To Live, the principalId is "dynamodb.amazonaws.com".
+        /// </summary>
+        public string PrincipalId { get; set; } = "";
+
+        /// <summary>
+        /// The type of the identity.
+        /// For Time To Live, the type is "Service".
+        /// </summary>
+        public string Type { get; set; } = "";
+    }
+}
