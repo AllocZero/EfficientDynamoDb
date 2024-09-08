f581cfda24ee8c624c10743ecb6a33d40bb68629
Mykhailo Matviiv <firenero94@gmail.com>
2024-09-08 17:59:02 +0200

    WIP: OpResult

--- src/EfficientDynamoDb/Exceptions/ResourceNotFoundException.cs
+++ src/EfficientDynamoDb/Exceptions/ResourceNotFoundException.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Runtime.Serialization;
+using EfficientDynamoDb.Operations;
 
 namespace EfficientDynamoDb.Exceptions
 {
@@ -10,6 +10,10 @@ namespace EfficientDynamoDb.Exceptions
     /// </summary>
     public class ResourceNotFoundException : DdbException
     {
+        internal override OpErrorType OpErrorType => ErrorType;
+        
+        internal static OpErrorType ErrorType => OpErrorType.ResourceNotFound;
+        
         public ResourceNotFoundException(string message) : base(message)
         {
         }
