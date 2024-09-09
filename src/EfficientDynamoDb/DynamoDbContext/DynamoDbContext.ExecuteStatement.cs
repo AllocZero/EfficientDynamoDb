4fa4526c060038c6706e4c06fcfdb7a7fe926bca
Chris Perez <chrispc.dev@gmail.com>
2024-09-09 12:06:19 -0600

    add ExecuteStatement in DynamoDbContext

---
+++ src/EfficientDynamoDb/DynamoDbContext/DynamoDbContext.ExecuteStatement.cs
@@ -0,0 +1,23 @@
+﻿using EfficientDynamoDb.Internal.Operations.ExecuteStatement;
+using EfficientDynamoDb.Operations.ExecuteStatement;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EfficientDynamoDb
+{
+    public partial class DynamoDbContext
+    {
+        public async Task<List<TEntity>> ExecuteStatementAsync<TEntity>(ExecuteStatementRequest request, CancellationToken cancellationToken = default) where TEntity : class
+        {
+            var httpContent = new ExecuteStatementRequestHttpContent(request);
+            using var response = await Api.SendAsync(Config, httpContent, cancellationToken).ConfigureAwait(false);
+            var result = await ReadAsync<ExecuteStatementEntityResponse<TEntity>>(response, cancellationToken).ConfigureAwait(false);
+            var entities = new List<TEntity>(result.Items.Count);
+            foreach (var item in result.Items)
+                entities.Add(item);
+
+            return entities;
+        }
+    }
+}
