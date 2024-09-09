9495be29598221a1531b3401d790182a247bdc9c
Chris Perez <chrispc.dev@gmail.com>
2024-09-09 12:06:31 -0600

    add BatchExecuteStatement in DynamoDbContext

---
+++ src/EfficientDynamoDb/DynamoDbContext/DynamoDbContext.BatchExecuteStatement.cs
@@ -0,0 +1,23 @@
+﻿using EfficientDynamoDb.Internal.Operations.BatchExecuteStatement;
+using EfficientDynamoDb.Operations.BatchExecuteStatement;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EfficientDynamoDb
+{
+    public partial class DynamoDbContext
+    {
+        public async Task<List<TEntity?>> BatchExecuteStatementAsync<TEntity>(BatchExecuteStatementRequest request, CancellationToken cancellationToken = default) where TEntity : class
+        {
+            var httpContent = new BatchExecuteStatementRequestHttpContent(request);
+            using var response = await Api.SendAsync(Config, httpContent, cancellationToken).ConfigureAwait(false);
+            var result = await ReadAsync<BatchExecuteStatementEntityResponse<TEntity>>(response, cancellationToken).ConfigureAwait(false);
+            var entities = new List<TEntity?>(result.Responses.Count);
+            foreach (var item in result.Responses)
+                entities.Add(item.Item);
+
+            return entities;
+        }
+    }
+}
