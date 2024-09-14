3aff5affc90e0f1def254c4d8d4b72f7759fda72
Mykhailo Matviiv <firenero94@gmail.com>
2024-09-14 14:49:50 +0200

    UpdateItem

--- src/EfficientDynamoDb/Operations/UpdateItem/UpdateRequestBuilder.cs
+++ src/EfficientDynamoDb/Operations/UpdateItem/UpdateRequestBuilder.cs
@@ -36,13 +36,13 @@ namespace EfficientDynamoDb.Operations.UpdateItem
         public async Task<TEntity?> ToItemAsync(CancellationToken cancellationToken = default)
         {
             var classInfo = _context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity));
-            return await _context.UpdateItemAsync<TEntity>(classInfo, _node, cancellationToken).ConfigureAwait(false);
+            return await _context.UpdateItemAsync<TEntity>(classInfo, _node, cancellationToken).EnsureSuccess().ConfigureAwait(false);
         }
 
         public async Task<UpdateItemEntityResponse<TEntity>> ToResponseAsync(CancellationToken cancellationToken = default)
         {
             var classInfo = _context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity));
-            return await _context.UpdateItemResponseAsync<TEntity>(classInfo, _node, cancellationToken).ConfigureAwait(false);
+            return await _context.UpdateItemResponseAsync<TEntity>(classInfo, _node, cancellationToken).EnsureSuccess().ConfigureAwait(false);
         }
         
         public IAttributeUpdate<IUpdateEntityRequestBuilder<TEntity>, TEntity, TProperty> On<TProperty>(Expression<Func<TEntity, TProperty>> expression) =>
@@ -73,12 +73,42 @@ namespace EfficientDynamoDb.Operations.UpdateItem
             new UpdateEntityRequestBuilder<TEntity>(_context, new PartitionKeyNode<TPk>(pk, _node));
 
         public IUpdateDocumentRequestBuilder<TEntity> AsDocument() => new UpdateDocumentRequestBuilder<TEntity>(_context, _node);
+        
+        public ISuppressedUpdateItemEntityRequestBuilder<TEntity> SuppressThrowing() => new SuppressedUpdateEntityRequestBuilder<TEntity>(_context, _node);
 
         IUpdateEntityRequestBuilder<TEntity> IUpdateItemBuilder<IUpdateEntityRequestBuilder<TEntity>>.Create(UpdateBase update, BuilderNodeType nodeType) =>
             new UpdateEntityRequestBuilder<TEntity>(_context, new UpdateAttributeNode(update, nodeType, _node));
     }
     
-     internal sealed class UpdateDocumentRequestBuilder<TEntity> : IUpdateDocumentRequestBuilder<TEntity> where TEntity : class
+    internal sealed class SuppressedUpdateEntityRequestBuilder<TEntity> : ISuppressedUpdateItemEntityRequestBuilder<TEntity> where TEntity : class
+    {
+        private readonly DynamoDbContext _context;
+        private readonly BuilderNode? _node;
+
+        internal SuppressedUpdateEntityRequestBuilder(DynamoDbContext context, BuilderNode? node)
+        {
+            _context = context;
+            _node = node;
+        }
+
+        public async Task<OpResult> ExecuteAsync(CancellationToken cancellationToken = default)
+        {
+            var classInfo = _context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity));
+            var result = await _context.UpdateItemAsync<TEntity>(classInfo, _node, cancellationToken).ConfigureAwait(false);
+            return result.DiscardValue();
+        }
+
+        public Task<OpResult<TEntity?>> ToItemAsync(CancellationToken cancellationToken = default) =>
+            _context.UpdateItemAsync<TEntity>(_context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity)), _node, cancellationToken);
+
+        public Task<OpResult<UpdateItemEntityResponse<TEntity>>> ToResponseAsync(CancellationToken cancellationToken = default) =>  
+            _context.UpdateItemResponseAsync<TEntity>(_context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity)), _node, cancellationToken);
+
+        ISuppressedUpdateItemEntityRequestBuilder<TEntity> IUpdateItemBuilder<ISuppressedUpdateItemEntityRequestBuilder<TEntity>>.Create(UpdateBase update,
+            BuilderNodeType nodeType) => new SuppressedUpdateEntityRequestBuilder<TEntity>(_context, _node);
+    }
+    
+    internal sealed class UpdateDocumentRequestBuilder<TEntity> : IUpdateDocumentRequestBuilder<TEntity> where TEntity : class
     {
         private readonly DynamoDbContext _context;
         private readonly BuilderNode? _node;
@@ -104,13 +134,13 @@ namespace EfficientDynamoDb.Operations.UpdateItem
         public async Task<Document?> ToItemAsync(CancellationToken cancellationToken = default)
         {
             var classInfo = _context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity));
-            return await _context.UpdateItemAsync<Document>(classInfo, _node, cancellationToken).ConfigureAwait(false);
+            return await _context.UpdateItemAsync<Document>(classInfo, _node, cancellationToken).EnsureSuccess().ConfigureAwait(false);
         }
 
         public async Task<UpdateItemEntityResponse<Document>> ToResponseAsync(CancellationToken cancellationToken = default)
         {
             var classInfo = _context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity));
-            return await _context.UpdateItemResponseAsync<Document>(classInfo, _node, cancellationToken).ConfigureAwait(false);
+            return await _context.UpdateItemResponseAsync<Document>(classInfo, _node, cancellationToken).EnsureSuccess().ConfigureAwait(false);
         }
 
         public IAttributeUpdate<IUpdateDocumentRequestBuilder<TEntity>, TEntity, TProperty> On<TProperty>(Expression<Func<TEntity, TProperty>> expression) =>
@@ -136,8 +166,38 @@ namespace EfficientDynamoDb.Operations.UpdateItem
 
         public IUpdateDocumentRequestBuilder<TEntity> WithPrimaryKey<TPk>(TPk pk) =>
             new UpdateDocumentRequestBuilder<TEntity>(_context, new PartitionKeyNode<TPk>(pk, _node));
+        
+        public ISuppressedUpdateItemDocumentRequestBuilder<TEntity> SuppressThrowing() => new SuppressedUpdateDocumentRequestBuilder<TEntity>(_context, _node);
 
         IUpdateDocumentRequestBuilder<TEntity> IUpdateItemBuilder<IUpdateDocumentRequestBuilder<TEntity>>.Create(UpdateBase update, BuilderNodeType nodeType) =>
             new UpdateDocumentRequestBuilder<TEntity>(_context, new UpdateAttributeNode(update, nodeType, _node));
+    }
+    
+    internal sealed class SuppressedUpdateDocumentRequestBuilder<TEntity> : ISuppressedUpdateItemDocumentRequestBuilder<TEntity> where TEntity : class
+    {
+        private readonly DynamoDbContext _context;
+        private readonly BuilderNode? _node;
+
+        internal SuppressedUpdateDocumentRequestBuilder(DynamoDbContext context, BuilderNode? node)
+        {
+            _context = context;
+            _node = node;
+        }
+
+        public async Task<OpResult> ExecuteAsync(CancellationToken cancellationToken = default)
+        {
+            var classInfo = _context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity));
+            var result = await _context.UpdateItemAsync<Document>(classInfo, _node, cancellationToken).ConfigureAwait(false);
+            return result.DiscardValue();
+        }
+
+        public Task<OpResult<Document?>> ToItemAsync(CancellationToken cancellationToken = default) =>
+            _context.UpdateItemAsync<Document>(_context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity)), _node, cancellationToken);
+
+        public Task<OpResult<UpdateItemEntityResponse<Document>>> ToResponseAsync(CancellationToken cancellationToken = default) =>  
+            _context.UpdateItemResponseAsync<Document>(_context.Config.Metadata.GetOrAddClassInfo(typeof(TEntity)), _node, cancellationToken);
+
+        ISuppressedUpdateItemDocumentRequestBuilder<TEntity> IUpdateItemBuilder<ISuppressedUpdateItemDocumentRequestBuilder<TEntity>>.Create(UpdateBase update,
+            BuilderNodeType nodeType) => new SuppressedUpdateDocumentRequestBuilder<TEntity>(_context, _node);
     }
 }
