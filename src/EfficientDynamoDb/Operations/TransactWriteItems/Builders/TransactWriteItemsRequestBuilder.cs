f5eb7b912ad6d86f71f7d057ee7bf0af80d2dd48
Mykhailo Matviiv <firenero94@gmail.com>
2024-09-14 22:09:10 +0200

    TransactWriteItem

--- src/EfficientDynamoDb/Operations/TransactWriteItems/Builders/TransactWriteItemsRequestBuilder.cs
+++ src/EfficientDynamoDb/Operations/TransactWriteItems/Builders/TransactWriteItemsRequestBuilder.cs
@@ -38,12 +38,32 @@ namespace EfficientDynamoDb.Operations.TransactWriteItems.Builders
         public ITransactWriteItemsRequestBuilder WithItems(IEnumerable<ITransactWriteItemBuilder> items) =>
             new TransactWriteItemsRequestBuilder(_context, new BatchItemsNode<ITransactWriteItemBuilder>(items, _node));
         
-        public Task ExecuteAsync(CancellationToken cancellationToken = default) => _context.TransactWriteItemsAsync(GetNode(), cancellationToken);
+        public Task ExecuteAsync(CancellationToken cancellationToken = default) => _context.TransactWriteItemsAsync(GetNode(), cancellationToken).EnsureSuccess();
 
         public Task<TransactWriteItemsEntityResponse> ToResponseAsync(CancellationToken cancellationToken = default) =>
+            _context.TransactWriteItemsResponseAsync(GetNode(), cancellationToken).EnsureSuccess();
+        
+        public ISuppressedTransactWriteItemsRequestBuilder SuppressThrowing() => new SuppressedTransactWriteItemsRequestBuilder(_context, _node);
+        
+        private BuilderNode GetNode() => _node ?? throw new DdbException("Can't execute empty transact write items request.");
+    }
+    
+    internal sealed class SuppressedTransactWriteItemsRequestBuilder : ISuppressedTransactWriteItemsRequestBuilder
+    {
+        private readonly DynamoDbContext _context;
+        private readonly BuilderNode? _node;
+
+        internal SuppressedTransactWriteItemsRequestBuilder(DynamoDbContext context, BuilderNode? node)
+        {
+            _context = context;
+            _node = node;
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken = default) => _context.TransactWriteItemsAsync(GetNode(), cancellationToken);
+
+        public Task<OpResult<TransactWriteItemsEntityResponse>> ToResponseAsync(CancellationToken cancellationToken = default) =>
             _context.TransactWriteItemsResponseAsync(GetNode(), cancellationToken);
 
-        
         private BuilderNode GetNode() => _node ?? throw new DdbException("Can't execute empty transact write items request.");
     }
 }
