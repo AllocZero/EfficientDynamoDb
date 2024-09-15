bc5d05aee2125aeba614da89515973a00bc3e996
Mykhailo Matviiv <firenero94@gmail.com>
2024-09-15 15:08:26 +0200

    BatchWriteItem

--- src/EfficientDynamoDb/Operations/BatchWriteItem/BatchWriteItemRequestBuilder.cs
+++ src/EfficientDynamoDb/Operations/BatchWriteItem/BatchWriteItemRequestBuilder.cs
@@ -36,9 +36,33 @@ namespace EfficientDynamoDb.Operations.BatchWriteItem
             new BatchWriteItemRequestBuilder(_context, new ReturnItemCollectionMetricsNode(returnItemCollectionMetrics, _node));
 
         public Task ExecuteAsync(CancellationToken cancellationToken = default) => 
-            _context.BatchWriteItemAsync(_node ?? throw new DdbException("Can't execute empty batch write item request."), cancellationToken);
+            _context.BatchWriteItemAsync(GetNode(), cancellationToken).EnsureSuccess();
 
         public Task<BatchWriteItemResponse> ToResponseAsync(CancellationToken cancellationToken = default) =>
-            _context.BatchWriteItemResponseAsync(_node ?? throw new DdbException("Can't execute empty batch write item request."), cancellationToken);
+            _context.BatchWriteItemResponseAsync(GetNode(), cancellationToken).EnsureSuccess();
+        
+        public ISuppressedBatchWriteItemRequestBuilder SuppressThrowing() => new SuppressedBatchWriteItemRequestBuilder(_context, _node);
+
+        private BuilderNode GetNode() => _node ?? throw new DdbException("Can't execute empty batch write item request.");
+    }
+    
+    internal sealed class SuppressedBatchWriteItemRequestBuilder : ISuppressedBatchWriteItemRequestBuilder
+    {
+        private readonly DynamoDbContext _context;
+        private readonly BuilderNode? _node;
+        
+        public SuppressedBatchWriteItemRequestBuilder(DynamoDbContext context, BuilderNode? node)
+        {
+            _context = context;
+            _node = node;
+        }
+
+        public Task<OpResult> ExecuteAsync(CancellationToken cancellationToken = default) => 
+            _context.BatchWriteItemAsync(GetNode(), cancellationToken);
+
+        public Task<OpResult<BatchWriteItemResponse>> ToResponseAsync(CancellationToken cancellationToken = default) =>
+            _context.BatchWriteItemResponseAsync(GetNode(), cancellationToken);
+        
+        private BuilderNode GetNode() => _node ?? throw new DdbException("Can't execute empty batch write item request.");
     }
 }
