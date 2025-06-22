379b9ba7891c29d999d09632cd4c0db05ef56477
Mykhailo Matviiv <firenero94@gmail.com>
2025-06-23 01:03:30 +0200

    Refactor OpResult and add tests

---
+++ src/EfficientDynamoDb/Operations/ResultHandling/OpErrorType.cs
@@ -0,0 +1,25 @@
+﻿namespace EfficientDynamoDb.Operations;
+
+public enum OpErrorType
+{
+    None,
+    Unknown,
+    ServiceUnavailable,
+    InternalServerError,
+    TransactionCanceled,
+    ConditionalCheckFailed,
+    ProvisionedThroughputExceeded,
+    AccessDenied,
+    IncompleteSignature,
+    ItemCollectionSizeLimitExceeded,
+    LimitExceeded,
+    MissingAuthenticationToken,
+    RequestLimitExceeded,
+    ResourceInUse,
+    ResourceNotFound,
+    Throttling,
+    UnrecognizedClient,
+    Validation,
+    IdempotentParameterMismatch,
+    TransactionInProgress
+}
---
+++ src/EfficientDynamoDb/Operations/ResultHandling/OpResult.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using EfficientDynamoDb.Exceptions;
+
+namespace EfficientDynamoDb.Operations;
+
+public interface IOpResult
+{
+    public OpErrorType ErrorType { get; }
+        
+    public DdbException? Exception { get; }
+        
+    [MemberNotNullWhen(false, nameof(Exception))]
+    public bool IsSuccess { get; }
+}
+
+[StructLayout(LayoutKind.Auto)]
+public readonly struct OpResult : IOpResult
+{
+    public OpErrorType ErrorType => Exception?.OpErrorType ?? OpErrorType.None;
+
+    public DdbException? Exception { get; }
+
+    public OpResult(DdbException? exception)
+    {
+        Exception = exception;
+    }
+
+    public void EnsureSuccess()
+    {
+        if (Exception is not null)
+            throw Exception;
+    }
+
+    [MemberNotNullWhen(false, nameof(Exception))]
+    public bool IsSuccess => Exception is null;
+}
+
+public readonly struct OpResult<T> : IOpResult
+{
+    public T? Value { get; }
+
+    public OpErrorType ErrorType => Exception?.OpErrorType ?? OpErrorType.None;
+        
+    public DdbException? Exception { get; }
+        
+    public OpResult(DdbException exception)
+    {
+        Value = default;
+        Exception = exception;
+    }
+        
+    public OpResult(T value)
+    {
+        Value = value;
+        Exception = null;
+    }
+
+    [MemberNotNullWhen(true, nameof(Value))]
+    public T EnsureSuccess()
+    {
+        if (ErrorType != OpErrorType.None && Exception is not null)
+            throw Exception;
+
+        return Value!;
+    }
+
+    [MemberNotNullWhen(false, nameof(Exception))]
+    public bool IsSuccess => Exception is null;
+        
+    public OpResult DiscardValue() => new(Exception);
+}
