7fd22897dabfee8648bcd8f388e08a26d6f3d8c5
Mykhailo Matviiv <firenero94@gmail.com>
2026-08-30 02:41:59 +0200

    Small fixes

--- src/EfficientDynamoDb.Tests/Internal/JsonConverters/DdbEnumJsonConverter.cs
+++ src/EfficientDynamoDb.Tests/Internal/JsonConverters/DdbEnumJsonConverter.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using EfficientDynamoDb.Internal.JsonConverters;
 using EfficientDynamoDb.Operations.DescribeTable.Models.Enums;
 using NUnit.Framework;
+using Shouldly;
 
 namespace EfficientDynamoDb.Tests.Internal.JsonConverters
 {
@@ -14,7 +15,6 @@ namespace EfficientDynamoDb.Tests.Internal.JsonConverters
 
         [TestCase(KeyType.Hash, "HASH")]
         [TestCase(SseType.Kms, "KMS")]
-        [TestCase(SseType.Aes256, "AES256")]
         [TestCase(StreamViewType.NewAndOldImages, "NEW_AND_OLD_IMAGES")]
         [TestCase(StreamViewType.KeysOnly, "KEYS_ONLY")]
         public void EnumDeserializationTest<T>(T enumValue, string jsonValue) where T : struct, Enum
@@ -22,21 +22,19 @@ namespace EfficientDynamoDb.Tests.Internal.JsonConverters
             var json = $"\"{jsonValue}\"";
 
             var result = JsonSerializer.Deserialize<T>(json, _options);
-
-            Assert.That(result, Is.EqualTo(enumValue));
+            result.ShouldBe(enumValue);
         }
-        
+
         [TestCase(KeyType.Hash, "HASH")]
         [TestCase(SseType.Kms, "KMS")]
-        [TestCase(SseType.Aes256, "AES256")]
         [TestCase(StreamViewType.NewAndOldImages, "NEW_AND_OLD_IMAGES")]
         [TestCase(StreamViewType.KeysOnly, "KEYS_ONLY")]
         public void EnumSerializationTest<T>(T enumValue, string expectedJsonValue) where T : struct, Enum
         {
             var result = JsonSerializer.Serialize(enumValue, _options);
             var expectedJson = $"\"{expectedJsonValue}\"";
-            
-            Assert.That(result, Is.EqualTo(expectedJson));
+
+            result.ShouldBe(expectedJson);
         }
     }
 }
